1364f9eeab7ac3efee3d00134bb787dd414264f7
limseongkyu <96x60812@naver.com>
2021-06-12 23:03:16 +0900

    [Add] kakao map api

--- src/main/java/com/loopy/web/IndexController.java
+++ src/main/java/com/loopy/web/IndexController.java
@@ -1,4 +1,15 @@
 package com.loopy.web;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
 public class IndexController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
 }
