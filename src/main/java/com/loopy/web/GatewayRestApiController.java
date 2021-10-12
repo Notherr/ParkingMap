4c853790ff13cd595c195c5da1c479ed1b5db51d
limseongkyu <96x60812@naver.com>
2021-10-12 22:10:43 +0900

    [Add] use post, get 동시에

--- src/main/java/com/loopy/web/GatewayRestApiController.java
+++ src/main/java/com/loopy/web/GatewayRestApiController.java
@@ -3,16 +3,15 @@ package com.loopy.web;
 
 import com.loopy.web.dto.WantRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Required;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 public class GatewayRestApiController {
 
-    @PostMapping("/want")
-    public String postWant(@RequestBody WantRequestDto requestDto) {
-        System.out.println(requestDto);
+    //@RequestMapping(value="/want", method={RequestMethod.GET, RequestMethod.POST})
+    @RequestMapping("/want")
+    public String postWant(WantRequestDto requestDto) {
         if (requestDto.getValue().equals("1")){
             return "ok";
         }
@@ -20,6 +19,5 @@ public class GatewayRestApiController {
             return "no";
         }
     }
-
 
 }
