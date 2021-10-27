31cb7c0acca7e3821785f26cff9f655e051a769f
limseongkyu <96x60812@naver.com>
2021-10-27 11:31:48 +0900

    [Add] h2

--- src/main/java/com/loopy/web/GatewayRestApiController.java
+++ src/main/java/com/loopy/web/GatewayRestApiController.java
@@ -9,12 +9,15 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class GatewayRestApiController {
 
-    String val = "0";
     //@RequestMapping(value="/want", method={RequestMethod.GET, RequestMethod.POST})
     @RequestMapping("/want")
     public String postWant(WantRequestDto requestDto) {
-        return requestDto.getValue();
-
+        if (requestDto.getValue().equals("1")){
+            return "ok";
+        }
+        else{
+            return "no";
+        }
     }
 
 }
